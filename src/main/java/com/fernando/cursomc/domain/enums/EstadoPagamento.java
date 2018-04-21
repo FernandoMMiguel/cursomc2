316319498ba0dabae089763cf19243e858253d80
Fernando Miguel <fernandomms@yahoo.com.br>
2018-04-20 22:20:17 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/fernando/cursomc/domain/enums/EstadoPagamento.java
@@ -0,0 +1,39 @@
+package com.fernando.cursomc.domain.enums;
+
+public enum EstadoPagamento {
+
+	PENDENTE(1, "Pendente"),
+	QUITADO(2, "Quitado"),
+	CANCELADO(3, "Cancelado");
+	
+	private int cod;
+	private String descricao;
+	
+	private EstadoPagamento(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	
+	public int getCod() {
+		return cod;
+	}
+	
+	public String getDescricao () {
+		return descricao;
+	}
+	
+	//Criado este metodo estatico poi assim ele pode rodar mesmo se não tiver objeto instanciado
+	public static EstadoPagamento toEnum(Integer cod) {
+		
+		if (cod == null) {
+			return null;
+		}
+		
+		for (EstadoPagamento x : EstadoPagamento.values()){
+			if(cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+}
