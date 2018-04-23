6b77753337b925504df323c9b23f650755489c2a
Fernando Miguel <fernandomms@yahoo.com.br>
2018-04-23 19:57:31 -0300

    Endpoint /pedido/{id} disponibilizado

--- src/main/java/com/fernando/cursomc/domain/ItemPedido.java
+++ src/main/java/com/fernando/cursomc/domain/ItemPedido.java
@@ -5,10 +5,14 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
@@ -30,12 +34,16 @@ public class ItemPedido implements Serializable{
 		this.preco = preco;
 	}
 
+	//ATENÇÂO: Tudo que começa co get ele intende que tem que serializar, sendo assim, 
+	// para evitar que esses metodos façam referencia ciclica precisar acrescentar @JsonIgnore
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
-	
 
+	//ATENÇÂO: Tudo que começa co get ele intende que tem que serializar, sendo assim, 
+	// para evitar que esses metodos façam referencia ciclica precisar acrescentar @JsonIgnore
+	@JsonIgnore
 	public Produto getProduto() {
 		return id.getProduto();
 	}
